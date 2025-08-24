aeef9f0fadf3df4a4eaf751603fcadbb2989a5fe
Cat C <catherinetcai@users.noreply.github.com>
2025-08-23 19:43:24 -0700

    Inventory file for AWS has to be aws_ec2.yaml name

--- terraform_aws/ansible.tf
+++ terraform_aws/ansible.tf
@@ -6,5 +6,5 @@ resource "local_file" "ansible_inventory" {
       ansible_ssm_bucket_name = aws_s3_bucket.ansible_ssm_bucket.bucket
     }
   )
-  filename = "../ansible/inventory/aws.yaml"
+  filename = "../ansible/inventory/aws_ec2.yaml"
 }
